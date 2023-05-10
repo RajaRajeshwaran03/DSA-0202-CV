23d1f1dd4343173f1a92417509381978a727ccfb
RajaRajeshwaran <114379354+RajaRajeshwaran03@users.noreply.github.com>
2023-05-10 12:59:13 +0530

    Update Linear Transformation.py

--- Linear Transformation.py
+++ Linear Transformation.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('C:/Users/mohammed rafik m/OneDrive/Pictures\Saved Pictures/WhatsApp Image 2022-03-18 at 6.54.12 AM.jpeg')
+image = cv2.imread('C:/Users/Raja/OneDrive/Pictures\Saved Pictures/WhatsApp Image 2022-03-18 at 6.54.12 AM.jpeg')
 source_points = np.array([[141, 131], [480, 159], [493, 630], [64, 601]], dtype=np.float32)
 target_points = np.array([[0, 0], [500, 0], [500, 500], [0, 500]], dtype=np.float32)
 
